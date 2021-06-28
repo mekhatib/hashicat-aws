e12dd8e7302a228683b9a765fe3efed30863b578
mekhatib <melkhatib@ziggo.nl>
2021-06-28 18:31:26 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,9 @@
 module "s3-bucket" {
   source  = "app.terraform.io/MAHIL-Test/s3-bucket/aws"
-  version = "2.5.0"
-
+  version = "2.2.0"
+  bucket = "my-s3-bucket"
   bucket_prefix = "ollanda"
   prefix = "ollandamahil"
-  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
