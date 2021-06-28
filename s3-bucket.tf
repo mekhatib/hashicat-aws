59aff2ec0a5ede66a41fceda74db8b1c47823e72
mekhatib <melkhatib@ziggo.nl>
2021-06-28 18:22:52 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,12 @@
 module "s3-bucket" {
   source  = "app.terraform.io/MAHIL-Test/s3-bucket/aws"
   version = "2.5.0"
-  # insert required variables here
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
